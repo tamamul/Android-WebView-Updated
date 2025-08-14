95620de6980646f1da4e8643cb275ae055bd7ac8
tamamul <36046825+tamamul@users.noreply.github.com>
2025-08-14 22:32:29 +0700

    Update MainActivity.kt

--- app/src/main/java/imrankst1221/website/in/webview/MainActivity.kt
+++ app/src/main/java/imrankst1221/website/in/webview/MainActivity.kt
@@ -1,1 +1,32 @@
-// MainActivity.kt
+package imrankst1221.website.in.webview
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+
+        // Ganti URL ini sesuai websitemu
+        val URL = "https://ngabsen.smkmaarif9kebumen.sch.id"
+        webView.loadUrl(URL)
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
